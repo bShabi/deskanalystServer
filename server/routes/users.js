260da26893c1f5ddbbddf7fe4ec4b67907dfaf55
Ben Shabi <49921117+bShabi@users.noreply.github.com>
2021-01-27 14:11:26 +0200

    heruko

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,12 @@ const { json } = require('body-parser');
 let account = require('../models/account.model');
 let team = require('../models/team.model');
 let moongose = require('mongoose')
+const mailgun = require("mailgun-js");
+
+
+
+
+
 
 router.route('/').get((req, res) => {
     account.find()
@@ -66,18 +72,20 @@ router.route('/remove/').post((req, res) => {
     console.log(userID, teamID)
     account.deleteOne({ _id: userID })
         .then(() => {
-            team.findById(teamID, function (err, docs) {
-                if (err) {
-                    console.log(err);
-                }
-                else {
-                    console.log(docs)
-                    docs.account.remove(userID)
-                    console.log(docs)
-                    docs.save()
-                }
-            });
-            res.json('User added')
+            if (teamID) {
+                team.findById(teamID, function (err, docs) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    else {
+                        console.log(docs)
+                        docs.account.remove(userID)
+                        console.log(docs)
+                        docs.save()
+                    }
+                });
+            }
+            res.json('User Removed')
         }
         )
 })
@@ -167,6 +175,14 @@ router.route('/find/:emailID').get((req, res) => {
 
 //Login 
 router.route('/login/').post((req, res) => {
+    const DOMAIN = 'sandbox9ae2caa30d294b27a95591d729530cb4.mailgun.org';
+    const mg = mailgun({ apiKey: 'eead9b31cbeeb3aa3ed5674b7c87ee62-07bc7b05-cdff2e99', domain: DOMAIN });
+    const data = {
+        from: "Mailgun Sandbox <postmaster@sandbox9ae2caa30d294b27a95591d729530cb4.mailgun.org>",
+        to: "bshabi1994@gmail.com",
+        subject: "Password Reset",
+        text: "Reset Password  A password reset event has been triggered. The password reset window is limited to two hours. If you do not reset your password within two hours, you will need to submit a new request.To complete the password reset process, visit the following link:"
+    };
 
     try {
         account.findOne({
@@ -183,11 +199,10 @@ router.route('/login/').post((req, res) => {
                         console.log(err);
                     }
                     else {
-                        console.log("in else")
                         newUser["teamName"] = obj.teamName
                         delete newUser.password
 
-                        console.log(newUser)
+                        // console.log(newUser)
                         res.json(newUser)
 
                     }
@@ -197,6 +212,20 @@ router.route('/login/').post((req, res) => {
     } catch (err) {
         console.log(err)
     }
+    console.log("berfore ms");
+
+    mg.messages().send(data, function (error, body) {
+        if (error)
+            console.log(error)
+        console.log("im mailgun");
+        console.log(body);
+    });
+    console.log("after");
+})
+router.route('/restPassword').get((req, res) => {
+    res.json("In")
+    console.log("berfore ms");
+
 })
 
 // account.findOne(emailID)
